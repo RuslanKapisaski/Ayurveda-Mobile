79384e77a561b2a954c4d2c97f2d8cdca67b8c74
Руслан Каписъски <ruslank1@abv.bg>
2026-02-03 20:26:19 +0200

    Update home navigator

--- src/navigation/HomeNavigator.jsx
+++ src/navigation/HomeNavigator.jsx
@@ -1,13 +1,35 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import HomeScreen from "../screens/HomeScreen";
+import DetailsScreen from "../screens/DetailsScreen";
+import ReservationScreen from "../screens/ReservationScreen";
+import QusetionUsScreen from "../screens/QuestionUsScreen";
 
 export default function HomeNavigator() {
   const Stack = createNativeStackNavigator();
 
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="DetailsScreen"
+        component={DetailsScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ReservationScreen"
+        component={ReservationScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="QuestioUsScreen"
+        component={QusetionUsScreen}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 }
