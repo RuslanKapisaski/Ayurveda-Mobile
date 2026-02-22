b9cd460ee76bf46581f1dc310ee26b4fa7e391e3
Руслан Каписъски <ruslank1@abv.bg>
2026-02-22 12:30:35 +0200

    Config firebase

---
+++ src/fireBaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+// Optionally import the services that you want to use
+import { getAuth } from "firebase/auth";
+// import {...} from 'firebase/database';
+// import {...} from 'firebase/firestore';
+// import {...} from 'firebase/functions';
+// import {...} from 'firebase/storage';
+
+// Initialize Firebase
+const firebaseConfig = JSON.parse(process.env.EXPO_PUBLIC_FIREBASE_CONFIG);
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
