885be2a1e96a32af6e9599f9a1450b06b93d437c
Руслан Каписъски <ruslank1@abv.bg>
2026-02-24 11:41:23 +0200

    Add custom button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,43 @@
+import { StyleSheet, TouchableOpacity, Text } from "react-native";
+
+export default function Button({ text, active }) {
+  return (
+    <TouchableOpacity
+      style={[
+        styles.button,
+        active ? styles.primaryButton : styles.secondaryButton,
+      ]}
+    >
+      <Text style={active ? styles.primaryText : styles.secondaryText}>
+        {text}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 20,
+    borderRadius: 16,
+    alignItems: "center",
+    shadowColor: "#000000",
+    shadowOffset: { width: 10, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+  },
+  primaryButton: {
+    backgroundColor: "#4A7C59",
+  },
+  secondaryButton: {
+    backgroundColor: "#ffffff",
+  },
+
+  primaryText: {
+    color: "#e5f9eb",
+    fontWeight: "600",
+  },
+  secondaryText: {
+    color: "#4A7C59",
+    fontWeight: "600",
+  },
+});
