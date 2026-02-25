1ccd004f70e442aac9334bae2fafece2353dc7f4
Руслан Каписъски <ruslank1@abv.bg>
2026-02-25 17:18:55 +0200

    Add therapies service

---
+++ src/services/therapiesService.js
@@ -0,0 +1,36 @@
+import { collection, getDoc, getDocs, doc } from "firebase/firestore";
+import { db } from "../fireBaseConfig";
+
+export async function getAll() {
+  try {
+    const therapiesSnapshot = await getDocs(collection(db, "therapies"));
+
+    return therapiesSnapshot.docs.map((therapy) => ({
+      id: therapy.id,
+      ...therapy.data(),
+    }));
+  } catch (error) {
+    console.error("Error fetching therapies:", error);
+    throw new Error("Failed to fetch therapies");
+  }
+}
+
+export async function getById(id) {
+  try {
+    const therapyRef = doc(db, "therapies", String(id));
+    const snapshot = await getDoc(therapyRef);
+
+    if (!snapshot.exists()) {
+      throw new Error("Therapy not found");
+    }
+
+    return { id: snapshot.id, ...snapshot.data() };
+  } catch (error) {
+    console.error("Error fetching therapy:", error, id);
+    throw new Error("Failed to fetch this therapy");
+  }
+}
+
+export async function getOne() {}
+export async function create() {}
+export async function edit() {}
