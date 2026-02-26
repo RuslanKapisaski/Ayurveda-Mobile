9977adc9d9912b6d9114d72788dbaa74f627212b
Руслан Каписъски <ruslank1@abv.bg>
2026-02-26 10:00:26 +0200

    Add therapies navigator

--- src/navigation/TherapiesNavigator.jsx
+++ src/navigation/TherapiesNavigator.jsx
@@ -2,11 +2,18 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import TherapiesScreen from "../screens/threapies/TherapiesScreen";
 import TherapiesDetailsScreen from "../screens/threapies/TherapiesDetailsScreen";
-import BookingScreen from "../screens/threapies/BookingScreen";
+import BookingScreen from "../screens/BookingScreen";
 
 export default function TherapiesNavigator() {
   const Stack = createNativeStackNavigator();
 
   return (
+    <Stack.Navigator>
+      <Stack.Screen name="Therapies" component={TherapiesScreen} />
+
+      <Stack.Screen name="TherapyDetails" component={TherapiesDetailsScreen} />
+
+      <Stack.Screen name="Booking" component={BookingScreen} />
+    </Stack.Navigator>
   );
 }
