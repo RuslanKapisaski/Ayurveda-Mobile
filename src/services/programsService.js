a8713c9cf08609f16426da1e0205e7157465eb8b
Руслан Каписъски <ruslank1@abv.bg>
2026-02-26 09:09:18 +0200

    Add program service

---
+++ src/services/programsService.js
@@ -0,0 +1,40 @@
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "../fireBaseConfig";
+
+export async function getAll() {
+  try {
+    const programsSnapshot = await getDocs(collection(db, "programs"));
+
+    return programsSnapshot.docs.map((program) => ({
+      id: program.id,
+      ...program.data(),
+    }));
+  } catch (error) {
+    console.error(`Error fetching programs ${error.message}`);
+    throw new Error(`Failed to fetch therapies, ${error.message}`);
+  }
+}
+
+export async function getById(id) {
+  try {
+    const programRef = doc(db, "programs", String(id));
+    const snapshot = await getDoc(programRef);
+
+    if (!snapshot.exists()) {
+      throw new Error("Program not found");
+    }
+
+    return { id: snapshot.id, ...snapshot.data() };
+  } catch (error) {
+    console.error(`Error fetching prgorams ${error.message}`);
+    throw new Error(`Failed to fetch this prgoram ${error.message}`);
+  }
+}
