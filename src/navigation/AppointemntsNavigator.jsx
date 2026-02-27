f9b64fb02d7b7793861dc981f2abb2382ed14b8c
Руслан Каписъски <ruslank1@abv.bg>
2026-02-27 19:46:48 +0200

    Add appointments stack navigator

---
+++ src/navigation/AppointemntsNavigator.jsx
@@ -0,0 +1,9 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+
+export default function AppointmentsNavigator() {
+  const Stack = createNativeStackNavigator();
+
+  return (
+  );
+}
