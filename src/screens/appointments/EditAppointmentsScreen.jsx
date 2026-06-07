2c6a23f437d21dcf24157e16c0eef8f80276dcc1
Руслан Каписъски <ruslank1@abv.bg>
2026-06-07 15:38:20 +0300

    Update EditAppointmentsScreen

--- src/screens/appointments/EditAppointmentsScreen.jsx
+++ src/screens/appointments/EditAppointmentsScreen.jsx
@@ -1,11 +1,37 @@
-import { useState } from "react";
-import { View, StyleSheet, Alert, ActivityIndicator } from "react-native";
+import { Image } from "expo-image";
+import { Text, View, StyleSheet, Alert, ActivityIndicator } from "react-native";
 import * as appointmentsService from "../../services/appointmentsService";
 import Calendar from "../../components/Calendar";
+import { useState, useEffect } from "react";
+import { useTheme } from "../../contexts/theme/useTheme";
 
 export default function EditAppointmentScreen({ route, navigation }) {
-  const { appointment } = route.params;
+  const { data } = route.params;
   const [isLoading, setIsLoading] = useState(false);
+  const [appointment, setAppointment] = useState(null);
+  const { theme } = useTheme()
+
+  useEffect(() => {
+    const loadAppointment = async () => {
+      try {
+        const result = await appointmentsService.getById(
+          data.itemId,
+          data.type,
+          data.userId
+        );
+
+        setAppointment({
+          ...result,
+          appointmentId: data.id,
+        });
+        
+      } catch (err) {
+        Alert.alert("Error", err.message);
+      }
+    };
+
+    loadAppointment();
+  }, []);
 
   const onEditHandler = (date) => {
     Alert.alert(
@@ -22,14 +48,11 @@ export default function EditAppointmentScreen({ route, navigation }) {
           onPress: async () => {
             try {
               setIsLoading(true);
-
-              await appointmentsService.edit(appointment, {
-                date,
-              });
+              await appointmentsService.edit({ ...appointment, id: data.id }, { date });
 
               Alert.alert("Success", "Appointment updated successfully!");
 
-              navigation.navigate("Appointments");
+              navigation.pop(1)
             } catch (err) {
               Alert.alert("Error", err.message);
             } finally {
@@ -42,7 +65,7 @@ export default function EditAppointmentScreen({ route, navigation }) {
     );
   };
 
-  if (isLoading) {
+  if (!appointment) {
     return (
       <View style={styles.loader}>
         <ActivityIndicator size="large" />
@@ -52,7 +75,22 @@ export default function EditAppointmentScreen({ route, navigation }) {
 
   return (
     <View style={styles.container}>
-      <Calendar data={appointment} onPress={onEditHandler} />
+      <View style={[styles.card, { backgroundColor: theme.colors.card }]}>
+        <Image
+          source={{ uri: appointment?.imageUrl }}
+          style={styles.image}
+          contentFit="cover"
+        />
+
+        <Text style={[styles.title, { color: theme.colors.primary }]}>
+          {appointment.name || appointment.title}
+        </Text>
+      </View>
+
+      <Calendar
+        data={appointment}
+        onPress={onEditHandler}
+      />
     </View>
   );
 }
@@ -60,12 +98,34 @@ export default function EditAppointmentScreen({ route, navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignContent: "center",
-    justifyContent: "center",
+    padding: 16,
   },
+
   loader: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+
+  card: {
+    alignItems: "center",
+    alignSelf: "center",
+    width: 220,
+    margin: 20,
+    padding: 30,
+    borderRadius: 18,
+  },
+
+  image: {
+    width: 200,
+    height: 180,
+    borderRadius: 12,
+    marginBottom: 12,
+  },
+
+  title: {
+    fontSize: 16,
+    fontWeight: "600",
+    textAlign: "center",
   },
 });
